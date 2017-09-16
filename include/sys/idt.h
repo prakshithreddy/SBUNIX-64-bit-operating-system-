39c555afcbf8e01f37513b68e09cc5833ff79ed4
Prashanth <prashanths1992@gmail.com>
2017-09-16 17:38:48 -0400

    and

---
+++ include/sys/idt.h
@@ -0,0 +1,8 @@
+#ifndef _IDT_H
+#define _IDT_H
+
+
+void init_idt();
+
+
+#endif
---
+++ sys/idt.c
@@ -0,0 +1,48 @@
+#include <sys/defs.h>
+#include <sys/kprintf.h>
+
+struct idt_descriptor {
+    uint16_t id_base_lo;
+    uint16_t id_sel;
+    uint8_t id_always0;
+    uint8_t id_flags;
+    uint16_t id_base_middle;
+    uint32_t id_base_high;
+    uint32_t id_always00;
+}__attribute__((packed));
+
+
+struct idtr_t {
+    uint16_t limit;
+    uint64_t base;
+}__attribute__((packed));
+
+
+struct idt_descriptor idt[256];
+static const struct idtr_t idtp = {(sizeof(struct idt_descriptor)*256)-1,(uint64_t)idt};
+
+void id_set_gate(uint8_t intr_num,uint64_t base_addr, uint8_t sel,uint8_t flags)
+{
+    idt[intr_num].id_flags = flags;
+    idt[intr_num].id_always0 = 0;
+    idt[intr_num].id_always00 = 0;
+    idt[intr_num].id_sel = sel;
+    
+    //ADDRESS TO THE INTERRUPT SERVICE ROUTINE METHOD
+    
+    idt[intr_num].id_base_lo = (base_addr & 0xFFFF);
+    idt[intr_num].id_base_middle = (base_addr >> 16) & 0xFFFF;
+    idt[intr_num].id_base_high = (base_addr >> 32) & 0xFFFFFFFF;
+    
+}
+
+void _div0();
+
+void init_idt()
+{
+    //for(int i=0;i<256;i++)
+    //id_set_gate(0,(uint64_t)_div0,8,0x8E);
+    
+    __asm__ __volatile__("lidt %0" : : "m" (idtp));
+    
+}
