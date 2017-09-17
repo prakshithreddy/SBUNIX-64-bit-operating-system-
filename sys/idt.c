c7ec0c7a9b64dddf18fe79a12408fae64bdf38a4
Prashanth <prashanths1992@gmail.com>
2017-09-17 15:09:06 -0400

    andasd

--- sys/idt.c
+++ sys/idt.c
@@ -1,48 +1,9 @@
 #include <sys/defs.h>
 #include <sys/idt.h>
 #include <sys/pic.h>
+#include <sys/keyboard.h>
 #include <sys/kprintf.h>
 
-
-char kbdus[128] =
-{
-    0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
-    '9', '0', '-', '=', '\b',	/* Backspace */
-    '\t',			/* Tab */
-    'q', 'w', 'e', 'r',	/* 19 */
-    't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',	/* Enter key */
-    0,			/* 29   - Control */
-    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	/* 39 */
-    '\'', '`',   0,		/* Left shift */
-    '\\', 'z', 'x', 'c', 'v', 'b', 'n',			/* 49 */
-    'm', ',', '.', '/',   0,				/* Right shift */
-    '*',
-    0,	/* Alt */
-    ' ',	/* Space bar */
-    0,	/* Caps lock */
-    0,	/* 59 - F1 key ... > */
-    0,   0,   0,   0,   0,   0,   0,   0,
-    0,	/* < ... F10 */
-    0,	/* 69 - Num lock*/
-    0,	/* Scroll Lock */
-    0,	/* Home key */
-    0,	/* Up Arrow */
-    0,	/* Page Up */
-    '-',
-    0,	/* Left Arrow */
-    0,
-    0,	/* Right Arrow */
-    '+',
-    0,	/* 79 - End key*/
-    0,	/* Down Arrow */
-    0,	/* Page Down */
-    0,	/* Insert Key */
-    0,	/* Delete Key */
-    0,   0,   0,
-    0,	/* F11 Key */
-    0,	/* F12 Key */
-    0,	/* All other keys are undefined */
-};
 
 struct idt_descriptor {
     uint16_t id_base_lo;
@@ -79,26 +40,49 @@ void id_set_gate(uint8_t intr_num,uint64_t base_addr, uint8_t sel,uint8_t flags)
     
 }
 
-static int k =0;
+static int shift=0,control=0;
 
 void _key_press_handler(){
     
     k++;
     unsigned char a;
     
-
-    
     a = inb(0x60);
     
     kprintf("\n%d \n",a);
-    
-    if(a==42)
-    {
-        kprintf("shift was pressed");
+
+    if(a<128) {
+        
+        //shift
+        if(a == 42)
+        {
+            shift =1;
+        }
+        //control
+        else if ( a == 29 )
+        {
+            control = 1;
+        }
+        else {
+        
+            if (shift == 1)
+            {
+                shift = 0;
+                kprintf("SHIFT+%c (%c)",kbdus[a],CAPS_kdbus[a]);
+            }
+            else if (control =1)
+            {
+                control=0;
+                kprintf("Control+%c",kbdus[a]);
+                
+            }
+            else
+            {
+                kprintf("%c",kbdus[a]);
+            }
+        
+        }
     }
-    
-    if(k%2==1)
-    {kprintf("%c",kbdus[a]);}
     
     
     outb(0x20,0x20);
