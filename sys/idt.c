18fe8bab4a6185ca6ce688d74c4f2ff7ece03e4a
Prashanth <prashanths1992@gmail.com>
2017-09-17 19:45:16 -0400

    and

--- sys/idt.c
+++ sys/idt.c
@@ -164,7 +164,7 @@ void _rtc_intr_hndlr(){
     
     outb(0x0A,0x70);
     
-    unsigned char rtc_second=NULL,rtc_minute=NULL,regb,rtc_hour=NULL;
+    unsigned char rtc_second='T',rtc_minute='T',regb,rtc_hour='T';
     
     //check if update in progress
     
