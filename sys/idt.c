039b8cd9aa5b5a92b30fa965abeb3e8f7dcd1bbe
Prashanth <prashanths1992@gmail.com>
2017-09-17 17:17:29 -0400

    and

--- sys/idt.c
+++ sys/idt.c
@@ -158,11 +158,11 @@ void _timer_intr_hdlr(){
         
         unsigned char rtc_minute;
         outb(0x02,0x70);
-        rtc_second = inb(0x71);
+        rtc_minute = inb(0x71);
         
         unsigned char rtc_hour;
         outb(0x04,0x70);
-        rtc_second = inb(0x71);
+        rtc_hour = inb(0x71);
         
         time_bar(rtc_hour,rtc_minute,rtc_second,0XF0);
         
