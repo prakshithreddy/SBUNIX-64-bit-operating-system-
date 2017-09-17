ec40614eac0e8284a72398937e0acc00485af454
Prashanth <prashanths1992@gmail.com>
2017-09-16 21:13:03 -0400

    and

--- sys/idt.c
+++ sys/idt.c
@@ -4,7 +4,7 @@
 #include <sys/kprintf.h>
 
 
-unsigned char kbdus[128] =
+char kbdus[128] =
 {
     0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
     '9', '0', '-', '=', '\b',	/* Backspace */
