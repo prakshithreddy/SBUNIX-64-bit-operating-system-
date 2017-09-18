a2871c3a37dc1962580a2fa46ff1bac3b2791359
Prashanth <prashanths1992@gmail.com>
2017-09-17 20:27:28 -0400

    and

--- sys/kprintf.c
+++ sys/kprintf.c
@@ -88,7 +88,7 @@ void time_bar(int hours,int minutes,int seconds,int color){
     char *m_pointer1 = itoa(m_pointer,minutes);
     char s_pointer[6];
     char *s_pointer1 = itoa(s_pointer,seconds);
-    char *string = "TIME_LOCAL(UTC-4h): ";
+    char *string = "TIME(UTC-4h): ";
     char s1[35];
     char *s = time_concatenate(s1,string,h_pointer1,m_pointer1,s_pointer1);
     //int z=0;
