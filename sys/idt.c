00a409b9c90085eceda8685da9a0a09c1eb06917
Prashanth <prashanths1992@gmail.com>
2017-09-17 20:16:42 -0400

    sad

--- sys/idt.c
+++ sys/idt.c
@@ -194,12 +194,12 @@ void _rtc_intr_hndlr(){
         
     if (!(regb & 0x04)) {
         //rtc_second=(rtc_second & 0x0F)+((rtc_second/16)*10);
-        //rtc_minute=(rtc_minute & 0x0F)+ ((rtc_minute/16)*10);
-       // rtc_hour=((rtc_hour & 0x0F) +(((rtc_hour & 0x70)/16)*10));
+        rtc_minute=(rtc_minute & 0x0F)+ ((rtc_minute/16)*10);
+        rtc_hour=((rtc_hour & 0x0F) +(((rtc_hour & 0x70)/16)*10));
     }
     
     if (!(regb & 0x02) && (rtc_hour & 0x80)) {
-           // rtc_hour = ((rtc_hour & 0x7F) + 12) % 24;
+            rtc_hour = ((rtc_hour & 0x7F) + 12) % 24;
     }
         
     kprintf("\n%d:%d:%d\n",rtc_hour,rtc_minute,rtc_second);
