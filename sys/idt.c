621b5edcb17980a9b3e78468a816a8507561a9a0
Prashanth <prashanths1992@gmail.com>
2017-11-14 00:36:14 -0500

    asd

--- sys/idt.c
+++ sys/idt.c
@@ -325,6 +325,9 @@ void _hndlr_isr13(registers_t regs){
 
 void _hndlr_isr14(registers_t regs){
     kprintf("0x0E    Page fault");
+    uint64_t pagefaultAt;
+    __asm__ __volatile__("movq %%cr2, %%rax; movq %%rax, %0;":"=m"(pagefaultAt)::"%rax");
+    kprintf("%x",pagefaultAt);
     while(1);
 }
 
