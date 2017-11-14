cc9d81940f30aad079e8df815bf2db3a81387a8d
Prashanth <prashanths1992@gmail.com>
2017-11-14 00:29:35 -0500

    asd

--- sys/idt.c
+++ sys/idt.c
@@ -362,6 +362,29 @@ void _hndlr_isr21(registers_t regs){
     while(1);
 }
 
+void _isr0();
+void _isr1();
+void _isr2();
+void _isr3();
+void _isr4();
+void _isr5();
+void _isr6();
+void _isr7();
+void _isr8();
+void _isr9();
+void _isr10();
+void _isr11();
+void _isr12();
+void _isr13();
+void _isr14();
+void _isr15();
+void _isr16();
+void _isr17();
+void _isr18();
+void _isr19();
+void _isr20();
+void _isr21();
+
 
 void _key_board_intr();
 
