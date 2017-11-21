2f67be6ecf3c12c363cb50ea3ab4d48fc2475602
Prashanth <prashanths1992@gmail.com>
2017-11-21 00:32:42 -0500

    asd

--- include/sys/syscall.h
+++ include/sys/syscall.h
@@ -3,6 +3,14 @@
 
 #include<sys/defs.h>
 
+struct dBlock
+{
+    void* a;
+    void* b;
+};
+
+void* syscall5(void* syscall_number,void* param1,void* param2,void* param3,void* param4,void* param5);
+
 extern uint64_t userRSP;
 extern uint64_t kernelRSP;
 
