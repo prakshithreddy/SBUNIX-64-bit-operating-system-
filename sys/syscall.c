88eed73cdf698bb0cf17b5578de08f49cd6bd05b
Prashanth <prashanths1992@gmail.com>
2017-11-21 03:38:27 -0500

    asd

--- sys/syscall.c
+++ sys/syscall.c
@@ -18,13 +18,13 @@ uint64_t readMSR(uint32_t msrId)
     return (uint64_t)msrHigh<<32|(uint64_t)msrLow;
 }
 
-int64_t syscallHandler(int64_t paramA,int64_t paramB,int64_t paramC,int64_t paramD,int64_t paramE,int64_t paramF,int64_t syscallNum) {
+uint64_t syscallHandler(uint64_t paramA,uint64_t paramB,uint64_t paramC,uint64_t paramD,uint64_t paramE,uint64_t paramF,uint64_t syscallNum) {
     kprintf("%d",syscallNum);
     return 100;
 }
 
-int64_t syscall(int64_t syscallNum,int64_t param1,int64_t param2,int64_t param3,int64_t param4,int64_t param5,int64_t param6) {
-    int64_t returnValue;
+uint64_t syscall(uint64_t syscallNum,uint64_t param1,uint64_t param2,uint64_t param3,uint64_t param4,uint64_t param5,uint64_t param6) {
+    uint64_t returnValue;
     __asm__ volatile ("movq %1, %%rax; movq %2, %%rdi; movq %3, %%rsi; movq %4, %%rdx; movq %5, %%r10; movq %6, %%r8;movq %7, %%r9;syscall;movq %%rax, %0;":"=g"(returnValue):"g"(syscallNum),"g"(param1),"g"(param2),"g"(param3),"g"(param4),"g"(param5),"g"(param6):"%rax","%rdi","%rsi","%rdx","%r10","%r8","%r9","memory");//clobber list
     return returnValue;
 }
@@ -45,13 +45,15 @@ void initSyscalls()
     kernelRSP = (uint64_t*)kmalloc();
     //STEP1: set the system call extension bit (SCE bit) to 1;
     uint64_t sce = readMSR(MSR_EFER);
-    sce |= (0x1);
+    sce |= (0x1); //enable syscalls
     writeMSR(sce,MSR_EFER);
+    
+    writeMSR(1<<9,MSR_SFMASK);
     
     writeMSR((uint64_t)0x8<<32|(uint64_t)0x1B<<48,MSR_STAR);
     
     writeMSR((uint64_t)_syscallEntry,MSR_LSTAR);
     
-    writeMSR(1<<9,MSR_SFMASK);
+    
     
 }
