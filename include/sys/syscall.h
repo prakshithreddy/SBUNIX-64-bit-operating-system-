0f261e61a680dc23937088fbc77851d3c46ba048
Prashanth <prashanths1992@gmail.com>
2017-11-21 00:48:45 -0500

    asd

--- include/sys/syscall.h
+++ include/sys/syscall.h
@@ -11,7 +11,7 @@ extern uint64_t kernelRSP;
 void initSyscalls();
 
 uint64_t readMSR(uint32_t msrId);
-void writeMSR(unint64_t value,uint32_t msrId);
+void writeMSR(uint64_t value,uint32_t msrId);
 
 int64_t syscall(int64_t syscallNum,int64_t param1,int64_t param2,int64_t param3,int64_t param4,int64_t param5,int64_t param6);
 
