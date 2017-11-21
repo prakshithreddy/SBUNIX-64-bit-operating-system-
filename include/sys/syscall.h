95f63c32c3626dcbeab1b9dd3ce1ed9664cf8358
Prashanth <prashanths1992@gmail.com>
2017-11-21 05:57:45 -0500

    asd

--- include/sys/syscall.h
+++ include/sys/syscall.h
@@ -13,5 +13,5 @@ void initSyscalls();
 uint64_t readMSR(uint32_t msrAddr);
 void writeMSR(uint64_t value,uint32_t msrAddr);
 
-uint64_t syscall(uint64_t syscallNum,uint64_t param1,uint64_t param2,uint64_t param3,uint64_t param4,uint64_t param5,uint64_t param6);
+uint64_t syscall(uint64_t param1,uint64_t param2,uint64_t param3,uint64_t param4,uint64_t param5,uint64_t param6,uint64_t syscallNum);
 #endif
