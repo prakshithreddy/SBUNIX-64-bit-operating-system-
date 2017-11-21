9527e9297c8f59e16acbedbe234e05e2666b17ad
Prashanth <prashanths1992@gmail.com>
2017-11-21 00:47:34 -0500

    asd

--- include/sys/syscall.h
+++ include/sys/syscall.h
@@ -1,11 +1,6 @@
 #ifndef _SYSCALL_H
 #define _SYSCALL_H
-
-struct dBlock
-{
-    void* a;
-    void* b;
-};
+#include<sys/defs.h>
 
 void* syscall5(void* syscall_number,void* param1,void* param2,void* param3,void* param4,void* param5);
 
