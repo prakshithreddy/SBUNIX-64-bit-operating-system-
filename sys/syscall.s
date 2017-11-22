2db52b1f6fd4a19baa3561f316211ff08cdf8796
Prashanth <prashanths1992@gmail.com>
2017-11-22 01:44:23 -0500

    asd

--- sys/syscall.s
+++ sys/syscall.s
@@ -11,37 +11,37 @@ _syscallEntry:
     movq %rsp, userRSP
     movq (kernelRSP),%rsp
     pushq (userRSP)
+    pushq %rcx
     pushq %r11
-    pushq %rdx
+    movq %r10,%rcx
     pushq %rbx
+    pushq %rdx
     pushq %rsi
     pushq %rdi
-    pushq %r13
     pushq %r8
     pushq %r9
-    pushq %r12
     pushq %r10
-    pushq %r15
+    pushq %r12
+    pushq %r13
     pushq %r14
+    pushq %r15
     pushq %rbp
-    pushq %rcx
     pushq %rax
-    movq %r10,%rcx
     call syscallHandler
     addq $0x8,%rsp // rax contains the returnValue, so not popping
-    pushq %rcx
     popq %rbp
-    popq %r14
     popq %r15
-    popq %r10
+    popq %r14
+    popq %r13
     popq %r12
+    popq %r10
     popq %r9
     popq %r8
-    popq %r13
     popq %rdi
     popq %rsi
-    popq %rbx
     popq %rdx
+    popq %rbx
     popq %r11
+    popq %rcx
     popq %rsp
     sysretq
