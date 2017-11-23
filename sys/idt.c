8ecf2ecb977a1eb978f26ea0e8b2fae5ee23bed8
Prashanth <prashanths1992@gmail.com>
2017-11-23 00:47:19 -0500

    eqweqweqweqweq

--- sys/idt.c
+++ sys/idt.c
@@ -57,8 +57,8 @@ void _key_press_handler(registers_t regs){
     unsigned char a;
     
     uint64_t temp;
-    kprintf("In User Space1........");
-    __asm__ __volatile__("movq (%%rsp), %%rax; movq %%rax, %0;":"=m"(temp)::"%rax");
+    //kprintf("In User Space1........");
+    __asm__ __volatile__("movq %%rsp, %%rax; movq %%rax, %0;":"=m"(temp)::"%rax");
     kprintf("%p ", temp);
     
     a = inb(0x60);
