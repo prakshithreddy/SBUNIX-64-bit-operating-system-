908b14f66cc7dec2863568c6f7a6eb9008891389
Prashanth <prashanths1992@gmail.com>
2017-11-25 18:26:53 -0500

    asd

--- sys/main.c
+++ sys/main.c
@@ -42,6 +42,7 @@ void start(uint32_t *modulep, void *physbase, void *physfree)
   printALLDrivers();
   enablePaging();
     char *s = (void*)(0x0);
+    kprintf("%p",s);
     *s='a';
     
   kprintf("***************************Paging Enabled***************************\n");
