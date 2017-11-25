1e8f6ba2cdd263ef5b2af8d0f68af940c519f330
Prashanth <prashanths1992@gmail.com>
2017-11-24 21:29:38 -0500

    sad

--- crt/crt1.c
+++ crt/crt1.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include<sys/defs.h>
 
+#define null (void*)0
+
 void _start(void) {
   // call main() and exit() here
     uint64_t* rsp;
     __asm__ __volatile__("movq %%rsp,%0;":"=r"(rsp)::);
-  main(*(rsp+1)),(void*)0,(void*)0);
+  main(*(rsp+1),null,null);
 }
 
