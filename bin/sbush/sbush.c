db8d84f607774333fc7d24440e0cf08cdb48e128
Prashanth <prashanths1992@gmail.com>
2017-11-24 21:28:50 -0500

    asd

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 
-int main(int val){
+int main(int argc, char *argv[], char *envp[]){
     val+=1;
         while(1);
 }
--- include/stdlib.h
+++ include/stdlib.h
@@ -2,8 +2,8 @@
 #define _STDLIB_H
 #include<sys/defs.h>
 
-int main(int val);
-//int main(int argc, char *argv[], char *envp[]);
+//int main(int val);
+int main(int argc, char *argv[], char *envp[]);
 
 void exit(int status);
 void *malloc(size_t size);
