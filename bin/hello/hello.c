5b8d5f0616dd26bf57f05104ca0c6a8a4305c9ed
Prashanth <prashanths1992@gmail.com>
2017-11-24 21:21:46 -0500

    asd

--- bin/hello/hello.c
+++ bin/hello/hello.c
@@ -1,19 +1,20 @@
-#include <stdlib.h>
+//#include <stdlib.h>
+//
+//
+//int main(int val){
+//        /*int x=0;
+//        int i=0;
+//        char *s=(char *)(0xffffffff800b8000);
+//        while(x<100){
+//                x++;
+//                *s=i+'0';
+//                s++;
+//                i++;
+//                if(i==10){
+//                        i=0;
+//                }
+//        }*/
+//        
+//        while(1);
+//}
 
-
-int main(int val){
-        /*int x=0;
-        int i=0;
-        char *s=(char *)(0xffffffff800b8000);
-        while(x<100){
-                x++;
-                *s=i+'0';
-                s++;
-                i++;
-                if(i==10){
-                        i=0;
-                }
-        }*/
-        
-        while(1);
-}
