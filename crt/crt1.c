022d47f4ea15ee0881291bbbe53d662b4d57f1b6
Prashanth <prashanths1992@gmail.com>
2017-11-24 21:27:57 -0500

    asd

--- crt/crt1.c
+++ crt/crt1.c
@@ -5,6 +5,6 @@ void _start(void) {
   // call main() and exit() here
     uint64_t* rsp;
     __asm__ __volatile__("movq %%rsp,%0;":"=r"(rsp)::);
-  main(*(rsp+1));
+  main(*(rsp+1)),void* 0,void* 0);
 }
 
