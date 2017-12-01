aa3e00fcc12e9b84a4fa7ed4a078ef30ba59676e
Prashanth <prashanths1992@gmail.com>
2017-12-01 02:22:25 -0500

    asd

--- sys/syscall.c
+++ sys/syscall.c
@@ -8,7 +8,7 @@
 #define MSR_EFER 0xC0000080
 #define MSR_STAR 0xC0000081
 #define MSR_LSTAR 0xC0000082
-#define MSR_SFMASK  0xC0000084
+#define MSR_SFMASK 0xC0000084
 
 uint64_t* userRSP = 0;
 uint64_t* userRIP = 0;
