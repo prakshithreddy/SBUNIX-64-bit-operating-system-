27efceef05750f92489acc9fb41f1990ab658569
Prashanth <prashanths1992@gmail.com>
2017-11-30 20:55:47 -0500

    ad

--- sys/syscall.c
+++ sys/syscall.c
@@ -35,12 +35,6 @@ uint64_t syscallHandler(uint64_t paramA,uint64_t paramB,uint64_t paramC,uint64_t
     
 }
 
-uint64_t syscall(uint64_t syscallNum,uint64_t param1,uint64_t param2,uint64_t param3,uint64_t param4,uint64_t param5,uint64_t param6) {
-    uint64_t returnValue;
-    __asm__ volatile ("movq %1, %%rax; movq %2, %%rdi; movq %3, %%rsi; movq %4, %%rdx; movq %5, %%r10; movq %6, %%r8;movq %7, %%r9;syscall;movq %%rax, %0;":"=g"(returnValue):"g"(syscallNum),"g"(param1),"g"(param2),"g"(param3),"g"(param4),"g"(param5),"g"(param6):"%rax","%rdi","%rsi","%rdx","%r10","%r8","%r9","memory");//clobber list
-    return returnValue;
-}
-
 void writeMSR(uint64_t value,uint32_t msrAddr)
 {
     uint32_t msrLow;
