f2b6270ae7512a2c40ed6bfd875d90377a4f183d
Prashanth <prashanths1992@gmail.com>
2017-11-30 22:52:40 -0500

    asd

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -4,14 +4,15 @@
 uint64_t syscall(uint64_t syscallNum,uint64_t param1,uint64_t param2,uint64_t param3,uint64_t param4,uint64_t param5,uint64_t param6) {
     uint64_t returnValue;
     __asm__ volatile ("movq %1, %%rax; movq %2, %%rdi; movq %3, %%rsi; movq %4, %%rdx; movq %5, %%r10; movq %6, %%r8;movq %7, %%r9;syscall;movq %%rax, %0;":"=g"(returnValue):"g"(syscallNum),"g"(param1),"g"(param2),"g"(param3),"g"(param4),"g"(param5),"g"(param6):"%rax","%rdi","%rsi","%rdx","%r10","%r8","%r9","memory");//clobber list
-    syscall(99,1,2,3,4,5,6);
     return returnValue;
 }
 
 int main(int argc, char *argv[], char *envp[]){
     argc+=1;
     
-        syscall(syscall(1,1,2,3,4,5,6),1,2,3,4,5,6);
+     syscall(1,1,2,3,4,5,6)
+    syscall(10,1,2,3,4,5,6)
+    syscall(11,1,2,3,4,5,6)
 //    king fine.... need to debug the mischevious child//
 //    //kprintf("%d",retVal);
 //    syscall(argc,1,2,3,4,5,6);
