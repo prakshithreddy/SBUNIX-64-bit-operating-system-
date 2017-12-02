a542aa5980f47226a74984731cd1a1b76ee90d9b
Prashanth <prashanths1992@gmail.com>
2017-12-01 23:52:00 -0500

    asd

--- sys/task.s
+++ sys/task.s
@@ -114,32 +114,95 @@ _prepareInitialKernelStack:
     movq currentRSP,%rsp //restore stack pointer
     retq
 
+//_moveToNextProcess:
+//
+//    cli
+//
+////    movq %rax,(%rdi)
+////    movq %rbx,8(%rdi)
+////    movq %rcx,16(%rdi)
+////    movq %rdx,24(%rdi)
+////    movq %rsi,32(%rdi)
+////    movq %rdi,40(%rdi)
+////    movq %rbp,56(%rdi)
+//    /*movq %rip,64(%rdi)*/
+////    pushfq
+////    pop %rax
+////    movq %rax,72(%rdi)
+////    movq %cr3,%rax
+////    movq %rax,80(%rdi)
+////    movq %rsp,48(%rdi)
+////    pushfq
+////    pop %rax
+////    movq %rax,72(%rdi)
+////    movq %cr3,%rax
+////    movq %rax,80(%rdi)
+////    //movq %rsp,%rax
+////    //addq $80,%rax
+////    movq %rsp,88(%rdi)
+//
+//    //done : saved the state of the first process
+//
+//    movq 80(%rsi),%rax //loading the user cr3 val
+//    movq %rax, %cr3
+//
+//    movq $0x23,%rax
+//    mov %rax,%ds
+//    mov %rax,%es   //setting segment registers
+//    movq %rax,%fs
+//    mov %rax,%gs
+//
+//
+//    //    movq 48(%rsi),%rax
+//    //    movq %rax,%rsp
+//    //    pushq $0x23
+//    //    pushq %rax
+//    //    movq 72(%rsi),%rax
+//    //    pushq %rax
+//    //    popq %rax
+//    //    orq $0x200,%rax
+//    //    pushq %rax
+//    //    pushq $0x2B
+//    //    movq 64(%rsi),%rax
+//    //    pushq %rax
+//    movq 88(%rsi),%rsp
+//    subq 104(%rsi),%rsp
+//    movq 8(%rsi),%rbx
+//    movq 16(%rsi),%rcx
+//    movq 24(%rsi),%rdx
+//    /*movq 32(%rsi),%rsi*/
+//    movq 40(%rsi),%rdi
+//    movq 56(%rsi),%rbp
+//    movq $0,%rax
+//
+//    iretq
+
 _moveToNextProcess:
 
     cli
 
-//    movq %rax,(%rdi)
-//    movq %rbx,8(%rdi)
-//    movq %rcx,16(%rdi)
-//    movq %rdx,24(%rdi)
-//    movq %rsi,32(%rdi)
-//    movq %rdi,40(%rdi)
-//    movq %rbp,56(%rdi)
+    //    movq %rax,(%rdi)
+    //    movq %rbx,8(%rdi)
+    //    movq %rcx,16(%rdi)
+    //    movq %rdx,24(%rdi)
+    //    movq %rsi,32(%rdi)
+    //    movq %rdi,40(%rdi)
+    //    movq %rbp,56(%rdi)
     /*movq %rip,64(%rdi)*/
-//    pushfq
-//    pop %rax
-//    movq %rax,72(%rdi)
-//    movq %cr3,%rax
-//    movq %rax,80(%rdi)
-//    movq %rsp,48(%rdi)
-//    pushfq
-//    pop %rax
-//    movq %rax,72(%rdi)
-//    movq %cr3,%rax
-//    movq %rax,80(%rdi)
-//    //movq %rsp,%rax
-//    //addq $80,%rax
-//    movq %rsp,88(%rdi)
+    //    pushfq
+    //    pop %rax
+    //    movq %rax,72(%rdi)
+    //    movq %cr3,%rax
+    //    movq %rax,80(%rdi)
+    //    movq %rsp,48(%rdi)
+    //    pushfq
+    //    pop %rax
+    //    movq %rax,72(%rdi)
+    //    movq %cr3,%rax
+    //    movq %rax,80(%rdi)
+    //    //movq %rsp,%rax
+    //    //addq $80,%rax
+    //    movq %rsp,88(%rdi)
 
     //done : saved the state of the first process
 
@@ -170,12 +233,13 @@ _moveToNextProcess:
     movq 8(%rsi),%rbx
     movq 16(%rsi),%rcx
     movq 24(%rsi),%rdx
+    movq 72(%rsi),%r11
     /*movq 32(%rsi),%rsi*/
     movq 40(%rsi),%rdi
     movq 56(%rsi),%rbp
     movq $0,%rax
 
-    iretq
+    sysret
 
 _pushVal:
     movq %rsp,currentRSP // will be restored later
