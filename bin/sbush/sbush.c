84cf2f378cd35b58c41cef202fd107a103cb1e30
Prashanth <prashanths1992@gmail.com>
2017-12-03 03:36:56 -0500

    asd

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -18,20 +18,16 @@ int main(int argc, char *argv[], char *envp[]){
     {
         //99malloc syscall
         int* a = (int*)syscall(99,4096,0,0,0,0,0);
-        int* b = (int*)syscall(99,4096,0,0,0,0,0);
-        int* c = (int*)syscall(99,4096,0,0,0,0,0);
-        int* d = (int*)syscall(99,4096,0,0,0,0,0);
-        int* e = (int*)syscall(99,4096,0,0,0,0,0);
-        int* f = (int*)syscall(99,4096,0,0,0,0,0);
+        
       //  int* g = (int*)syscall(99,4096,0,0,0,0,0);
+        a1[0] = 786;
         
-        
-        while(1)syscall(10,(uint64_t)a,(uint64_t)b,(uint64_t)c,(uint64_t)d,(uint64_t)e,(uint64_t)f);
+        while(1)syscall(10,(uint64_t)a,a1[0],0,0,0,0);
     }
     else
     {
-          a1[1] = 10;
-        while(1)syscall(20,1,2,3,4,5,6);
+          a1[0] = 10;
+        while(1)syscall(20,a1[0],2,3,4,5,6);
     }
     
     //syscall(10,1,2,3,4,5,6);
