ff952242e388838716caf3dc6b1ba1f409822f3e
Prashanth <prashanths1992@gmail.com>
2017-12-03 00:20:42 -0500

    asd

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 
 
-uint64_t syscall(uint64_t syscallNum,uint64_t param1,uint64_t param2,uint64_t param3,uint64_t param4,uint64_t param5,uint64_t param6) {
+void* syscall(uint64_t syscallNum,uint64_t param1,uint64_t param2,uint64_t param3,uint64_t param4,uint64_t param5,uint64_t param6) {
     uint64_t returnValue;
     __asm__ volatile ("movq %1, %%rax; movq %2, %%rdi; movq %3, %%rsi; movq %4, %%rdx; movq %5, %%r10; movq %6, %%r8;movq %7, %%r9;syscall;movq %%rax, %0;":"=g"(returnValue):"g"(syscallNum),"g"(param1),"g"(param2),"g"(param3),"g"(param4),"g"(param5),"g"(param6):"%rax","%rdi","%rsi","%rdx","%r10","%r8","%r9","memory");//clobber list
     return returnValue;
