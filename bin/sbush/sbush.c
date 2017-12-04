175d7e67784f719c5f06062ee01af18a98dd55f3
Prashanth <prashanths1992@gmail.com>
2017-12-03 19:53:26 -0500

    asd

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 
 
-void* syscall(uint64_t syscallNum,uint64_t param1,uint64_t param2,uint64_t param3,uint64_t param4,uint64_t param5,uint64_t param6) {
+void* syscall(void* syscallNum,void* param1,void* param2,void* param3,void* param4,void* param5,void* param6) {
     void* returnValue;
     __asm__ volatile ("movq %1, %%rax; movq %2, %%rdi; movq %3, %%rsi; movq %4, %%rdx; movq %5, %%r10; movq %6, %%r8;movq %7, %%r9;syscall;movq %%rax, %0;":"=g"(returnValue):"g"(syscallNum),"g"(param1),"g"(param2),"g"(param3),"g"(param4),"g"(param5),"g"(param6):"%rax","%rdi","%rsi","%rdx","%r10","%r8","%r9","memory");//clobber list
     return returnValue;
@@ -17,8 +17,10 @@ int main(int argc, char *argv[], char *envp[]){
     if(pid == 0)
     {
 
-        int* a = (int*)syscall(99,2000,0,0,0,0,0);
+        char* a = (char*)syscall(99,10*sizeof(int),0,0,0,0,0);
         a1[3] = 786;
+        for(int i=0;i<10;i++) a[i] = char(65+i);
+        //syscall(78,)
         while(1)syscall(10,(uint64_t)a,a1[0],a1[1],a1[3],0,0);
     }
     else
