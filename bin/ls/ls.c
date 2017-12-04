3933db3aae11764550e0edab3a2f0f891609df26
Prashanth <prashanths1992@gmail.com>
2017-12-04 04:27:56 -0500

    asd

--- bin/ls/ls.c
+++ bin/ls/ls.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 
 
-void* syscall(uint64_t syscallNum,uint64_t param1,uint64_t param2,uint64_t param3,uint64_t param4,uint64_t param5,uint64_t param6) {
+void* syscall(void* syscallNum,void* param1,void* param2,void* param3,void* param4,void* param5,void* param6) {
     void* returnValue;
     __asm__ volatile ("movq %1, %%rax; movq %2, %%rdi; movq %3, %%rsi; movq %4, %%rdx; movq %5, %%r10; movq %6, %%r8;movq %7, %%r9;syscall;movq %%rax, %0;":"=g"(returnValue):"g"(syscallNum),"g"(param1),"g"(param2),"g"(param3),"g"(param4),"g"(param5),"g"(param6):"%rax","%rdi","%rsi","%rdx","%r10","%r8","%r9","memory");//clobber list
     return returnValue;
@@ -10,5 +10,5 @@ void* syscall(uint64_t syscallNum,uint64_t param1,uint64_t param2,uint64_t param
 int main(int argc, char *argv[], char *envp[]){
     
     
-    while(1)syscall((void*)54,(void*)(uint64_t)1,(void*)2,(void*)(uint64_t)3,(void*)(uint64_t)5,(void*)5,(void*)6);;
+    while(1)syscall((void*)54,(void*)(uint64_t)1,(void*)2,(void*)(uint64_t)3,(void*)(uint64_t)5,(void*)5,(void*)6);
 }
