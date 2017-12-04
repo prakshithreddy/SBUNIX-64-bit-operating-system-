d1540a4d5610028f311c1c2b3fd540de8506388a
Prashanth <prashanths1992@gmail.com>
2017-12-03 20:17:01 -0500

    asd

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[], char *envp[]){
         char* a = (char*)syscall((void*)99,(void*)(10*sizeof(int)),(void*)0,(void*)0,(void*)0,(void*)0,(void*)0);
         a1[3] = 786;
         for(int i=0;i<10;i++) a[i] = (char)(65+i);
-        //syscall(78,)
+        syscall((void*)78,(void*)a,(void*)a,(void*)a,(void*)1,(void*)2,(void*)3);
         while(1)syscall((void*)10,(void*)a,(void*)(uint64_t)a1[0],(void*)(uint64_t)a1[1],(void*)(uint64_t)a1[3],(void*)0,(void*)0);
     }
     else
