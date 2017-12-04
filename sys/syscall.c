e8ecc8aa6bbeffd0d276d05b416608fbf6324658
Prashanth <prashanths1992@gmail.com>
2017-12-04 06:02:01 -0500

    asd

--- sys/syscall.c
+++ sys/syscall.c
@@ -32,7 +32,7 @@ uint64_t readMSR(uint32_t msrAddr)
 
 void* syscallHandler(uint64_t paramA,uint64_t paramB,uint64_t paramC,uint64_t paramD,uint64_t paramE,uint64_t paramF,uint64_t syscallNum) {
     
-    kprintf("\n%d %d %s %d %d %d %d",syscallNum,paramA,((char*)paramB),paramC,paramD,paramE,paramF);
+    kprintf("\n%d %d %d %d %d %d %d",syscallNum,paramA,paramB,paramC,paramD,paramE,paramF);
     
     switch(syscallNum)
     {
