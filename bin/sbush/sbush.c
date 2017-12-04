d77d24b9120839790fceb9ea2763101b5cbff103
Prashanth <prashanths1992@gmail.com>
2017-12-03 22:02:52 -0500

    asd

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -17,13 +17,13 @@ int main(int argc, char *argv[], char *envp[]){
     if(pid == 0)
     {
         a1[3] = 786;
-        char** a = (char**)syscall((void*)99,(void*)(10*sizeof(char*)),(void*)0,(void*)0,(void*)0,(void*)0,(void*)0);
+        char* a = (char*)syscall((void*)99,(void*)(10*sizeof(char*)),(void*)0,(void*)0,(void*)0,(void*)0,(void*)0);
         
         for(int i=0;i<10;i++) a[i] = (char*)syscall((void*)99,(void*)(uint64_t)(10*sizeof(char)),(void*)0,(void*)0,(void*)0,(void*)0,(void*)0);
         
         for(int i=0;i<10;i++) a[i] = (char*)"a=test";
         
-        syscall((void*)78,(void*)a,(void*)a[0],(void*)a,(void*)1,(void*)2,(void*)3);
+        syscall((void*)78,(void*)a,(void*)a,(void*)a,(void*)1,(void*)2,(void*)3);
         while(1);//syscall((void*)10,(void*)a,(void*)(uint64_t)a1[0],(void*)(uint64_t)a1[1],(void*)(uint64_t)a1[3],(void*)0,(void*)0);
     }
     else
