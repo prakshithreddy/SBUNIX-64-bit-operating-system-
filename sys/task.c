a239033e2da7e38a8629cacee71a0d045dbdce8d
Prashanth <prashanths1992@gmail.com>
2017-12-05 02:07:51 -0500

    asd

--- sys/task.c
+++ sys/task.c
@@ -761,7 +761,7 @@ uint64_t malloc(uint64_t size)
 void* exec(void* path,void* args,void* envp)
 {
     
-    kprintf("%s %s %s\n",((char*)path),((char**)args)[1],((char**)envp)[2]);
+    kprintf("%s %s %s\n",((char*)path),((char**)args)[0],((char**)envp)[1]);
     
     Task *task = (Task*)kmalloc();
     uint64_t newCr3 = (uint64_t)getNewPML4ForUser();
