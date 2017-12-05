d77a8c93297b7528f8cf01a9b5627a664f7d92ba
Prashanth <prashanths1992@gmail.com>
2017-12-04 20:32:53 -0500

    asd

--- sys/task.c
+++ sys/task.c
@@ -571,7 +571,7 @@ uint64_t getPageNumFromAddr(uint64_t addr)
 
 void* printMe(void* path,void* args,void* envp)
 {
-    kprintf("%d %s %s\n",((uint64_t)path),(char*)args,(char*)envp);//,((char**)envp)[2]);
+    kprintf("%d\n",((uint64_t)path));//,(char*)args,(char*)envp);//,((char**)envp)[2]);
     return 0;
 }
 uint64_t malloc(uint64_t size)
