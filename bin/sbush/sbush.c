13ea2957f0098e2c454cd062c9ac26c441a6ac86
Prashanth <prashanths1992@gmail.com>
2017-12-05 00:54:47 -0500

    asd

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -17,7 +17,6 @@ int main(int argc, char *argv[], char *envp[]){
     if(pid == 0)
     {
         a1[3] = 786;
-        char* temp = (char*)"bin/ls";
         //syscall(10,(uint64_t)a,a1[0],a1[1],a1[3],0,0);
         //char *c=(char *)syscall(99,2000,0,0,0,0,0);
         //*c='\0';
@@ -26,20 +25,20 @@ int main(int argc, char *argv[], char *envp[]){
             //syscall(6,1,(uint64_t)c,0,0,0,0);
         //}
         char** a = (char**)syscall((void*)99,(void*)(4*sizeof(char*)),(void*)0,(void*)0,(void*)0,(void*)0,(void*)0);
-
-        //for(int i=0;i<1;i++)*/ //char* a = (char*)syscall((void*)99,(void*)(uint64_t)(10*sizeof(char)),(void*)0,(void*)0,(void*)0,(void*)0,(void*)0);
-
+        
+        //for(int i=0;i<10;i++) a[i] = (char*)syscall((void*)99,(void*)(uint64_t)(10*sizeof(char)),(void*)0,(void*)0,(void*)0,(void*)0,(void*)0);
+        
         for(int i=0;i<4;i++) a[i] = (char*)"a=kjhs";
         a[3] = (char*)"a=kjhsqqqqqqkajhskjdasd";
         a[0] = "bin/ls";
         
-        syscall((void*)78,(void*)temp,(void*)a,(void*)a,(void*)1,(void*)2,(void*)3);
+        syscall((void*)78,(void*)a[0],(void*)a,(void*)a,(void*)1,(void*)2,(void*)3);
         while(1);// syscall((void*)10,(void*)a,(void*)(uint64_t)a1[0],(void*)(uint64_t)a1[1],(void*)(uint64_t)a1[3],(void*)0,(void*)(uint64_t)argc);
     }
     else
     {
           a1[1] = 11;
-        int64_t i = (int64_t)syscall((void*)88,(void*)2,(void*)&a1[0],(void*)3,(void*)4,(void*)5,(void*)a1);
+        int64_t i = syscall((void*)88,(void*)4,(void*)&a1[0],(void*)3,(void*)4,(void*)5,(void*)a1);
         syscall((void*)420,(void*)(uint64_t)a1[0],(void*)0,(void*)0,(void*)(uint64_t)4,(void*)5,(void*)(int64_t)i);
         while(1);// syscall((void*)20,(void*)(uint64_t)a1[0],(void*)2,(void*)(uint64_t)a1[1],(void*)(uint64_t)a1[3],(void*)5,(void*)(uint64_t)argc);
     }
