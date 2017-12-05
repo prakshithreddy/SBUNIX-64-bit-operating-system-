ef8a14875f650548fedce050e41323c409ed1b03
Prashanth <prashanths1992@gmail.com>
2017-12-05 07:32:52 -0500

    asd

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -32,7 +32,7 @@ int main(int argc, char *argv[], char *envp[]){
 //        a[3] = (char*)"a=kjhsqqqqqqkajhskjdasd";
         //char* temp = (char*)"bin/ls";
         
-        syscall((void*)420,(void*)(uint64_t)argc,(void*)argv,(void*)envp,(void*)0,(void*)(uint64_t)4,(void*)5);
+       // syscall((void*)420,(void*)(uint64_t)argc,(void*)argv,(void*)envp,(void*)0,(void*)(uint64_t)4,(void*)5);
         
        // syscall((void*)78,(void*)temp,(void*)argv,(void*)envp,(void*)1,(void*)2,(void*)3);
         while(1);// syscall((void*)10,(void*)a,(void*)(uint64_t)a1[0],(void*)(uint64_t)a1[1],(void*)(uint64_t)a1[3],(void*)0,(void*)(uint64_t)argc);
@@ -43,7 +43,7 @@ int main(int argc, char *argv[], char *envp[]){
         char* temp = (char*)"bin/ls";
         //asdasd
        syscall((void*)88,(void*)2,(void*)&a1[0],(void*)3,(void*)4,(void*)5,(void*)a1);
-        syscall((void*)420,(void*)(uint64_t)argc,(void*)argv,(void*)envp,(void*)0,(void*)(uint64_t)4,(void*)5);//,(void*)(int64_t)i);
+      //  syscall((void*)420,(void*)(uint64_t)argc,(void*)argv,(void*)envp,(void*)0,(void*)(uint64_t)4,(void*)5);//,(void*)(int64_t)i);
         syscall((void*)78,(void*)temp,(void*)argv,(void*)envp,(void*)1,(void*)2,(void*)3);
         while(1);// syscall((void*)20,(void*)(uint64_t)a1[0],(void*)2,(void*)(uint64_t)a1[1],(void*)(uint64_t)a1[3],(void*)5,(void*)(uint64_t)argc);
     }
