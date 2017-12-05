6ec82be5f2aea6b0d1abe7339862b6424666bfda
Prashanth <prashanths1992@gmail.com>
2017-12-05 05:29:55 -0500

    asd

--- sys/task.c
+++ sys/task.c
@@ -205,7 +205,7 @@ void pushInitialParamstoStack(Task* task)
     envC-=get_kernbase();
     mapPageForUser(0x1000,(uint64_t)envC,task->regs.cr3+get_kernbase());
     envC+=get_kernbase();
-    kprintf("%d",*envC);
+    kprintf("assssssssss %d",*envC);
     
     
     int z = 0x21000;
