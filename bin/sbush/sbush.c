26a9a57df76877fd651c959f58dbfc861289421e
Prashanth <prashanths1992@gmail.com>
2017-12-05 20:15:51 -0500

    awe

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -2,13 +2,6 @@
 #include <unistd.h>
 #include<fcntl.h>
 
-
-void* syscall(void* syscallNum,void* param1,void* param2,void* param3,void* param4,void* param5,void* param6) {
-    void* returnValue;
-    __asm__ volatile ("movq %1, %%rax; movq %2, %%rdi; movq %3, %%rsi; movq %4, %%rdx; movq %5, %%r10; movq %6, %%r8;movq %7, %%r9;syscall;movq %%rax, %0;":"=g"(returnValue):"g"(syscallNum),"g"(param1),"g"(param2),"g"(param3),"g"(param4),"g"(param5),"g"(param6):"%rax","%rdi","%rsi","%rdx","%r10","%r8","%r9","memory");//clobber list
-    return returnValue;
-}
-
 int main(int argc, char *argv[], char *envp[]){
 
     char* a1 = (char *)syscall((void*)99,(void*)2000,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0);
