69bfb3db9f6103baa21d9bc4a418a295b6145815
prakshithreddy <rakshithreddy.polireddy@stonybrook.edu>
2017-12-06 02:36:10 -0500

    sbush started..

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -9,101 +9,15 @@ void* syscall(void* syscallNum,void* param1,void* param2,void* param3,void* para
     return returnValue;
 }
 
-int main(int argc, char *argv[], char *envp[]){
-    
-    if(argc>=4) while(1)syscall((void*)420,(void*)(uint64_t)argc,(void*)argv,(void*)envp,(void*)0,(void*)(uint64_t)4,(void*)5);
+void   puts(char *buf){
+  int count=0;
+  while(buf[count]!='\0') count++;
+  syscall((void*)(uint64_t)4,(void*)(uint64_t)1,(void*)buf,(void*)(uint64_t)count,0,0,0);
+}
 
-    int* a1 = (int*)syscall((void*)99,(void*)2000,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0);
-    //a1[0] = 10;
-    uint64_t pid = (uint64_t)syscall((void*)1,(void*)1,(void*)2,(void*)3,(void*)4,(void*)5,(void*)a1);
-    
-    if(pid == 0)
-    {
-        //a1[3] = 786;
-        
-        char* temp = "bin/ps";
-        
-        syscall((void*)78,(void*)temp,(void*)argv,(void*)envp,(void*)1,(void*)2,(void*)3);
-        //syscall(10,(uint64_t)a,a1[0],a1[1],a1[3],0,0);
-        //char *c=(char *)syscall(99,2000,0,0,0,0,0);
-        // *c='\0';
-        //while(1){
-            //syscall(5,0,(uint64_t)c,64,0,0,0);
-            //syscall(6,1,(uint64_t)c,0,0,0,0);
-        //}
-//        char** a = (char**)syscall((void*)99,(void*)(4*sizeof(char*)),(void*)0,(void*)0,(void*)0,(void*)0,(void*)0);
-//
-//        //for(int i=0;i<10;i++) a[i] = (char*)syscall((void*)99,(void*)(uint64_t)(10*sizeof(char)),(void*)0,(void*)0,(void*)0,(void*)0,(void*)0);
-//
-//        for(int i=0;i<4;i++) a[i] = (char*)"a=kjhs";
-//        a[3] = (char*)"a=kjhsqqqqqqkajhskjdasd";
-        //char* temp = (char*)"bin/ls";
-        
-       // syscall((void*)420,(void*)(uint64_t)argc,(void*)argv,(void*)envp,(void*)0,(void*)(uint64_t)4,(void*)5);
-        
-       // syscall((void*)78,(void*)temp,(void*)argv,(void*)envp,(void*)1,(void*)2,(void*)3);
-        //asd
-        while(1);//syscall((void*)420,(void*)(uint64_t)argc,(void*)argv,(void*)envp,(void*)0,(void*)(uint64_t)4,(void*)5);// syscall((void*)10,(void*)a,(void*)(uint64_t)a1[0],(void*)(uint64_t)a1[1],(void*)(uint64_t)a1[3],(void*)0,(void*)(uint64_t)argc);
-    }
-    else
-    {
-        syscall((void*)88,(void*)2,(void*)&a1[0],(void*)3,(void*)4,(void*)5,(void*)a1);
-        syscall((void*)89,(void*)a1,(void*)&a1[0],(void*)3,(void*)4,(void*)5,(void*)a1);
-        
-    }
-///<<<<<<< HEAD
-//    {*/
-//          //a1[1] = 11;
-//        char *f="test.txt";
-//        int fd = open(f,1);
-//        if(fd!=-1){
-//          read(fd,a1,5);
-//          write(1,a1,5);
-//          close(fd);
-//        }
-//        char* a2 = (char *)syscall((void*)99,(void*)2000,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0);
-//        getcwd(a2,20);
-//        write(1,a2,20);
-//        chdir("/bin");
-//        getcwd(a2,20);
-//        write(1,a2,20);
-//        chdir("lakalak");
-//        getcwd(a2,20);
-//        write(1,a2,20);
-//        chdir("../");
-//        getcwd(a2,20);
-//        write(1,a2,20);
-//        chdir("./lakalak/../lakalak/");
-//        getcwd(a2,20);
-//        write(1,a2,20);
-//        chdir("/usr/folder1/folder2/..");
-//        getcwd(a2,20);
-//        write(1,a2,20);
-//        //int64_t i = (int64_t)syscall((void*)88,(void*)2,(void*)&a1[0],(void*)3,(void*)4,(void*)5,(void*)a1);
-//        //syscall((void*)420,(void*)envp,(void*)0,(void*)0,(void*)(uint64_t)4,(void*)5,(void*)(int64_t)i);
-////=======
-//
-//          a1[1] = 11;
-//        char* temp = (char*)"bin/ls";
-//        //asdasd
-//       syscall((void*)88,(void*)2,(void*)&a1[0],(void*)3,(void*)4,(void*)5,(void*)a1);
-//      //  syscall((void*)420,(void*)(uint64_t)argc,(void*)argv,(void*)envp,(void*)0,(void*)(uint64_t)4,(void*)5);//,(void*)(int64_t)i);
-//
-////>>>>>>> c3037649dc43c2190cb8ee587a4268edaf2260c9
-//        while(1);// syscall((void*)20,(void*)(uint64_t)a1[0],(void*)2,(void*)(uint64_t)a1[1],(void*)(uint64_t)a1[3],(void*)5,(void*)(uint64_t)argc);
-//    //}
-//
-//    //syscall(10,1,2,3,4,5,6);
-//    //syscall(11,1,2,3,4,5,6);
-////    king fine.... need to debug the mischevious child//
-////    //kprintf("%d",retVal);
-////    syscall(argc,1,2,3,4,5,6);
-////    syscall(argc,1,2,3,4,5,6);
-////
-////    char *s = (void*)(0x0);
-////    //kprintf("%p",s);
-////    *s='a';
-////
-////    syscall(argc,1,2,3,4,5,6);
-    while(1);//syscall((void*)420,(void*)(uint64_t)argc,(void*)argv,(void*)envp,(void*)0,(void*)(uint64_t)4,(void*)5);
+int main(int argc, char *argv[], char *envp[]){
+
+    char *ps1="sbush>";
+    puts(ps1);
+    while(1);
 }
