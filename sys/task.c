981e3e94ab409dbf767e7d7e276456a1e07483f1
Prashanth <prashanths1992@gmail.com>
2017-12-05 23:42:34 -0500

    awe

--- sys/task.c
+++ sys/task.c
@@ -941,7 +941,7 @@ void* free(void* ptr)
         if(vmaTemp->next->v_start==(uint64_t)ptr)
         {
             eTemp->next = vmaTemp->next->next;
-            pageDeAllocator((void*)((vmaTemp->next - get_kernbase())&FRAME));
+            pageDeAllocator((void*)((uint64_t)(vmaTemp->next - get_kernbase())&FRAME));
             break;
         }
         eTemp=vmaTemp->next;
