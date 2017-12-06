4a6cba5d71ead4ba7afc164d655295322c26c429
Prashanth <prashanths1992@gmail.com>
2017-12-06 03:02:20 -0500

    asd

--- include/stdlib.h
+++ include/stdlib.h
@@ -6,7 +6,7 @@
 int main(int argc, char *argv[], char *envp[]);
 
 void exit(int status);
-void *malloc(size_t size);
+//void *malloc(size_t size);
 void free(void *ptr);
 int setenv(char *name,char *value, int overwrite);
 
