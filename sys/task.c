199bf365a58ac85f9de4960087a345ff84c6fe94
Prashanth <prashanths1992@gmail.com>
2017-12-07 05:10:50 -0500

    asd

--- sys/task.c
+++ sys/task.c
@@ -1235,7 +1235,7 @@ void initUserProcess()
     if(hello_entrypoint == 0) return;
     
     kprintf("Entry Point: %p\n",hello_entrypoint);
-    char* temp = "bun/sbush";
+    char* temp = "bin/sbush";
     
     
     int i=0;
